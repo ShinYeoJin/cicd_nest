7d0ef2638f0d3d062936291ad37647f4894ad4c5
ShinYeoJin <syj3072@naver.com>
2026-01-13 14:48:20 +0900

    cicdnest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,12 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "eqeqeq": "warn",
+      "no-var": "warn",
+      "camelcase": "warn",
     },
   },
 );
