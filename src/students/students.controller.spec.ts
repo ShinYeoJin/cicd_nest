acbdbd71ef0461f659ad1e131a213bb3b72a1b3e
ShinYeoJin <syj3072@naver.com>
2026-01-13 15:03:46 +0900

    cicdnest

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let mockRepo: any
 
   beforeEach(async () => {
+    mockRepo = {
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [
+        StudentsService, 
+        {useValue: mockRepo, provide: getRepositoryToken(Student)},
+      ],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
